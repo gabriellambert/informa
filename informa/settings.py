0d6305e0d9d47fd75c30efa9d415a1aad3752dc5
Gabriel Mussolin Lambert <gabriel.mussolin@gmail.com>
2021-02-19 11:05:51 -0300

    Configura o banco de dados

--- informa/settings.py
+++ informa/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'informa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'noticias',
+        'USER': 'postgres',
+        'PASSWORD': 'chessegg91',
+        'HOST': 'localhost'
     }
 }
 
