72860bbe759f074b6df272ae7c62016716e74f49
Gabriel Mussolin Lambert <gabriel.mussolin@gmail.com>
2021-02-20 09:01:41 -0300

    Ajusta o display de noticias no admin

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Noticia
 
-# Register your models here.
+class Lista_de_Noticias(admin.modelAdmin):
+    list_display = ('titulo', 'categoria', 'publicada')
+    search_fields = ('titulo',)
+    list_filter = ('categoria',)
+    list_editable = ('publicada',)
+
+admin.site.register(Noticia, Lista_de_Noticias)
