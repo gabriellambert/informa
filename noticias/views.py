305e8f4ffb9568ea8b0f96a6dbaeb69b75a2c7c9
Gabriel Mussolin Lambert <gabriel.mussolin@gmail.com>
2021-02-20 08:27:33 -0300

    Exibe as notícias dinamicamente

--- noticias/views.py
+++ noticias/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Noticia
 
 def index(request):
-    return render(request, 'index.html')
+    noticias = Noticia.objects.order_by('-data_publicacao').filter(publicada=True)
+    dados = {
+        'noticias': noticias,
+    }
+    return render(request, 'index.html', dados)
 
 def noticia(request):
     return render(request, 'noticia.html')
